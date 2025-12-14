0c1ba9d54ce0796fece2dd0bdb08fdc525b8273e
MarcosG2003 <marcosgarnica12@gmail.com>
2025-12-13 19:41:03 -0600

    added in player, with player.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import pygame
+import pygame 
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
@@ -8,13 +9,25 @@ def main():
     print("Screen width: %s"% SCREEN_WIDTH)
     print("Screen height: %s"% SCREEN_HEIGHT)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    fps = 60
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+
+        player.draw(screen)
+
         pygame.display.flip()
+        tim = clock.tick(fps)
+        dt = tim / 1000
+        
+    
 
 if __name__ == "__main__":
     main()
